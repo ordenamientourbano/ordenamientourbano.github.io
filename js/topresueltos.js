357030f75dab983b80e3bc7543fe618664db361e
ordenamientourbano <87704071+ordenamientourbano@users.noreply.github.com>
2021-07-21 14:17:12 -0300

    Add files via upload

---
+++ js/topresueltos.js
@@ -0,0 +1,468 @@
+
+var junio = {"mes":[
+	{
+		"TipoPuntoEstrategico": "Bromatologia",
+		"Pendientes": 3,
+		"Total": 35,
+		"Resueltos": 32
+	},
+	{
+		"TipoPuntoEstrategico": "Comercio",
+		"Pendientes": 263,
+		"Total": 575,
+		"Resueltos": 312
+	},
+	{
+		"TipoPuntoEstrategico": "Defensa Civil",
+		"Pendientes": 57,
+		"Total": 120,
+		"Resueltos": 63
+	},
+	{
+		"TipoPuntoEstrategico": "Desarrollo Social",
+		"Pendientes": 2,
+		"Total": 45,
+		"Resueltos": 43
+	},
+	{
+		"TipoPuntoEstrategico": "Ente Vial ",
+		"Pendientes": 182,
+		"Total": 276,
+		"Resueltos": 94
+	},
+	{
+		"TipoPuntoEstrategico": "Espacios Públicos",
+		"Pendientes": 7,
+		"Total": 9,
+		"Resueltos": 2
+	},
+	{
+		"TipoPuntoEstrategico": "Guardaparque",
+		"Pendientes": 3,
+		"Total": 50,
+		"Resueltos": 47
+	},
+	{
+		"TipoPuntoEstrategico": "Higiene Urbana",
+		"Pendientes": 573,
+		"Total": 3051,
+		"Resueltos": 2478
+	},
+	{
+		"TipoPuntoEstrategico": "Infraestructura",
+		"Pendientes": 34,
+		"Total": 34,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Mascotas",
+		"Pendientes": 29,
+		"Total": 49,
+		"Resueltos": 20
+	},
+	{
+		"TipoPuntoEstrategico": "Medio ambiente",
+		"Pendientes": 7,
+		"Total": 12,
+		"Resueltos": 5
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Privadas",
+		"Pendientes": 8,
+		"Total": 9,
+		"Resueltos": 1
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Sanitarias",
+		"Pendientes": 355,
+		"Total": 584,
+		"Resueltos": 229
+	},
+	{
+		"TipoPuntoEstrategico": "OMIC",
+		"Pendientes": 2,
+		"Total": 2,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Otros",
+		"Pendientes": 11,
+		"Total": 11,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Policia",
+		"Pendientes": 105,
+		"Total": 105,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Salud",
+		"Pendientes": 6,
+		"Total": 85,
+		"Resueltos": 79
+	},
+	{
+		"TipoPuntoEstrategico": "SEM",
+		"Pendientes": 0,
+		"Total": 10,
+		"Resueltos": 10
+	},
+	{
+		"TipoPuntoEstrategico": "Señalización",
+		"Pendientes": 2,
+		"Total": 11,
+		"Resueltos": 9
+	},
+	{
+		"TipoPuntoEstrategico": "Servicios Públicos",
+		"Pendientes": 50,
+		"Total": 109,
+		"Resueltos": 59
+	},
+	{
+		"TipoPuntoEstrategico": "Tasa Municipal",
+		"Pendientes": 2,
+		"Total": 2,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Tránsito",
+		"Pendientes": 46,
+		"Total": 79,
+		"Resueltos": 33
+	},
+	{
+		"TipoPuntoEstrategico": "Transporte",
+		"Pendientes": 1,
+		"Total": 4,
+		"Resueltos": 3
+	},
+	{
+		"TipoPuntoEstrategico": "Usina",
+		"Pendientes": 293,
+		"Total": 293,
+		"Resueltos": 0
+	}
+] }
+
+var mayo = {"mes":[
+	{
+		"TipoPuntoEstrategico": "Bromatologia",
+		"Pendientes": 0,
+		"Total": 30,
+		"Resueltos": 30
+	},
+	{
+		"TipoPuntoEstrategico": "Comercio",
+		"Pendientes": 241,
+		"Total": 410,
+		"Resueltos": 169
+	},
+	{
+		"TipoPuntoEstrategico": "Defensa Civil",
+		"Pendientes": 0,
+		"Total": 70,
+		"Resueltos": 70
+	},
+	{
+		"TipoPuntoEstrategico": "Desarrollo Social",
+		"Pendientes": 0,
+		"Total": 16,
+		"Resueltos": 16
+	},
+	{
+		"TipoPuntoEstrategico": "Ente Vial ",
+		"Pendientes": 88,
+		"Total": 589,
+		"Resueltos": 501
+	},
+	{
+		"TipoPuntoEstrategico": "Guardaparque",
+		"Pendientes": 0,
+		"Total": 3,
+		"Resueltos": 3
+	},
+	{
+		"TipoPuntoEstrategico": "Higiene Urbana",
+		"Pendientes": 0,
+		"Total": 2801,
+		"Resueltos": 2801
+	},
+	{
+		"TipoPuntoEstrategico": "Infraestructura",
+		"Pendientes": 27,
+		"Total": 27,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Mascotas",
+		"Pendientes": 4,
+		"Total": 21,
+		"Resueltos": 17
+	},
+	{
+		"TipoPuntoEstrategico": "Medio ambiente",
+		"Pendientes": 8,
+		"Total": 8,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Privadas",
+		"Pendientes": 0,
+		"Total": 8,
+		"Resueltos": 8
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Sanitarias",
+		"Pendientes": 265,
+		"Total": 386,
+		"Resueltos": 121
+	},
+	{
+		"TipoPuntoEstrategico": "Otros",
+		"Pendientes": 27,
+		"Total": 28,
+		"Resueltos": 1
+	},
+	{
+		"TipoPuntoEstrategico": "Policia",
+		"Pendientes": 85,
+		"Total": 85,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Salud",
+		"Pendientes": 12,
+		"Total": 81,
+		"Resueltos": 69
+	},
+	{
+		"TipoPuntoEstrategico": "SEM",
+		"Pendientes": 0,
+		"Total": 2,
+		"Resueltos": 2
+	},
+	{
+		"TipoPuntoEstrategico": "Señalización",
+		"Pendientes": 2,
+		"Total": 22,
+		"Resueltos": 20
+	},
+	{
+		"TipoPuntoEstrategico": "Servicios Públicos",
+		"Pendientes": 32,
+		"Total": 105,
+		"Resueltos": 73
+	},
+	{
+		"TipoPuntoEstrategico": "Tasa Municipal",
+		"Pendientes": 8,
+		"Total": 8,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Tránsito",
+		"Pendientes": 16,
+		"Total": 35,
+		"Resueltos": 19
+	},
+	{
+		"TipoPuntoEstrategico": "Usina",
+		"Pendientes": 169,
+		"Total": 169,
+		"Resueltos": 0
+	}
+] }
+var abril = {"mes":[
+	{
+		"TipoPuntoEstrategico": "Bromatologia",
+		"Pendientes": 0,
+		"Total": 12,
+		"Resueltos": 12
+	},
+	{
+		"TipoPuntoEstrategico": "Comercio",
+		"Pendientes": 340,
+		"Total": 348,
+		"Resueltos": 8
+	},
+	{
+		"TipoPuntoEstrategico": "Defensa Civil",
+		"Pendientes": 12,
+		"Total": 35,
+		"Resueltos": 23
+	},
+	{
+		"TipoPuntoEstrategico": "Desarrollo Social",
+		"Pendientes": 0,
+		"Total": 3,
+		"Resueltos": 3
+	},
+	{
+		"TipoPuntoEstrategico": "Ente Vial ",
+		"Pendientes": 150,
+		"Total": 228,
+		"Resueltos": 78
+	},
+	{
+		"TipoPuntoEstrategico": "Guardaparque",
+		"Pendientes": 0,
+		"Total": 6,
+		"Resueltos": 6
+	},
+	{
+		"TipoPuntoEstrategico": "Higiene Urbana",
+		"Pendientes": 0,
+		"Total": 5549,
+		"Resueltos": 5549
+	},
+	{
+		"TipoPuntoEstrategico": "Infraestructura",
+		"Pendientes": 46,
+		"Total": 46,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Mascotas",
+		"Pendientes": 0,
+		"Total": 67,
+		"Resueltos": 67
+	},
+	{
+		"TipoPuntoEstrategico": "Medio ambiente",
+		"Pendientes": 3,
+		"Total": 3,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Privadas",
+		"Pendientes": 0,
+		"Total": 1,
+		"Resueltos": 1
+	},
+	{
+		"TipoPuntoEstrategico": "Obras Sanitarias",
+		"Pendientes": 300,
+		"Total": 305,
+		"Resueltos": 5
+	},
+	{
+		"TipoPuntoEstrategico": "OMIC",
+		"Pendientes": 1,
+		"Total": 1,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Otros",
+		"Pendientes": 42,
+		"Total": 42,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Policia",
+		"Pendientes": 103,
+		"Total": 103,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Salud",
+		"Pendientes": 0,
+		"Total": 96,
+		"Resueltos": 96
+	},
+	{
+		"TipoPuntoEstrategico": "SEM",
+		"Pendientes": 0,
+		"Total": 7,
+		"Resueltos": 7
+	},
+	{
+		"TipoPuntoEstrategico": "Señalización",
+		"Pendientes": 1,
+		"Total": 3,
+		"Resueltos": 2
+	},
+	{
+		"TipoPuntoEstrategico": "Servicios Públicos",
+		"Pendientes": 51,
+		"Total": 114,
+		"Resueltos": 63
+	},
+	{
+		"TipoPuntoEstrategico": "Tasa Municipal",
+		"Pendientes": 1,
+		"Total": 1,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Tránsito",
+		"Pendientes": 56,
+		"Total": 56,
+		"Resueltos": 0
+	},
+	{
+		"TipoPuntoEstrategico": "Transporte",
+		"Pendientes": 6,
+		"Total": 11,
+		"Resueltos": 5
+	},
+	{
+		"TipoPuntoEstrategico": "Usina",
+		"Pendientes": 153,
+		"Total": 153,
+		"Resueltos": 0
+	}
+] }
+
+function cargarReclamos(mes){
+    var DatosJson = JSON.parse(JSON.stringify(mes));
+    console.log(DatosJson.mes.length);
+    DatosJson.mes.sort(function (a, b) {
+        if (a.Total > b.Total) {
+          return -1;
+        }
+        if (a.Total < b.Total) {
+          return 1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+    document.getElementById("Table").innerHTML="";
+    $("#Table").append('<thead><th>Área</th>'+
+ 	'<th>Reclamos</th>' + 
+ 	'<th>Resueltos</th></thead>');
+    for (i = 0; i < DatosJson.mes.length; i++){
+ 
+ $("#Table").append('<tr>' + 
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].TipoPuntoEstrategico + '</td>'+
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].Total + '</td>'+
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].Resueltos + '</td>'+'</tr>');
+    }
+}
+
+function cargarResueltos(mes){
+    var DatosJson = JSON.parse(JSON.stringify(mes));
+    console.log(DatosJson.mes.length);
+    DatosJson.mes.sort(function (a, b) {
+        if (a.Resueltos > b.Resueltos) {
+          return -1;
+        }
+        if (a.Resueltos < b.Resueltos) {
+          return 1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+    document.getElementById("Table").innerHTML="";
+    $("#Table").append('<thead><th>Área</th>'+
+ 	'<th>Reclamos</th>' + 
+ 	'<th>Resueltos</th></thead>');
+    for (i = 0; i < DatosJson.mes.length; i++){
+ 
+ $("#Table").append('<tr>' + 
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].TipoPuntoEstrategico + '</td>'+
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].Total + '</td>'+
+ 	'<td align="center" style="dislay: none;">' + DatosJson.mes[i].Resueltos + '</td>'+'</tr>');
+    }
+}
