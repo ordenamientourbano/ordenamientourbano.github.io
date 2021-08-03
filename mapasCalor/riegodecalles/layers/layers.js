d21f77f52f5f3c998f0682d8b0e47ecd4dc37e52
ordenamientourbano <87704071+ordenamientourbano@users.noreply.github.com>
2021-08-02 22:17:41 -0300

    Add files via upload

---
+++ mapasCalor/riegodecalles/layers/layers.js
@@ -0,0 +1,71 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_car_1 = new ol.layer.Tile({
+            'title': 'car',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
+            })
+        });
+var format_Calorporzonas_2 = new ol.format.GeoJSON();
+var features_Calorporzonas_2 = format_Calorporzonas_2.readFeatures(json_Calorporzonas_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Calorporzonas_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Calorporzonas_2.addFeatures(features_Calorporzonas_2);
+var lyr_Calorporzonas_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Calorporzonas_2, 
+                style: style_Calorporzonas_2,
+                interactive: true,
+    title: 'Calor por zonas<br />\
+    <img src="styles/legend/Calorporzonas_2_0.png" /> 0 - 0<br />\
+    <img src="styles/legend/Calorporzonas_2_1.png" /> 0 - 2<br />\
+    <img src="styles/legend/Calorporzonas_2_2.png" /> 2 - 4<br />\
+    <img src="styles/legend/Calorporzonas_2_3.png" /> 4 - 6<br />\
+    <img src="styles/legend/Calorporzonas_2_4.png" /> 6 - 10<br />'
+        });
+var format_Riegodecalles_3 = new ol.format.GeoJSON();
+var features_Riegodecalles_3 = format_Riegodecalles_3.readFeatures(json_Riegodecalles_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Riegodecalles_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Riegodecalles_3.addFeatures(features_Riegodecalles_3);
+var lyr_Riegodecalles_3 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Riegodecalles_3, 
+                style: style_Riegodecalles_3,
+                interactive: true,
+                title: '<img src="styles/legend/Riegodecalles_3.png" /> Riego de calles'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_car_1.setVisible(true);lyr_Calorporzonas_2.setVisible(true);lyr_Riegodecalles_3.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_car_1,lyr_Calorporzonas_2,lyr_Riegodecalles_3];
+lyr_Calorporzonas_2.set('fieldAliases', {'id': 'id', 'Nº Asist': 'Nº Asist', 'limpieza': 'limpieza', 'espaciosve': 'espaciosve', 'podapreventiva': 'podapreventiva', 'podas': 'podas', 'NUMPOINTS': 'NUMPOINTS', });
+lyr_Riegodecalles_3.set('fieldAliases', {'Id': 'Id', 'Latitud': 'Latitud', 'Longitud': 'Longitud', 'Direccion': 'Direccion', 'Ubicacion': 'Ubicacion', 'Numeracion': 'Numeracion', 'direcc': 'direcc', 'pais': 'pais', 'provincia': 'provincia', 'Localidad': 'Localidad', 'TipoAlerta': 'TipoAlerta', 'Descripcio': 'Descripcio', 'Prioridad': 'Prioridad', 'FechaHoraI': 'FechaHoraI', 'UsuarioCre': 'UsuarioCre', 'EsPublico': 'EsPublico', 'EstadoPubl': 'EstadoPubl', 'Observacio': 'Observacio', 'Zona': 'Zona', 'EstadoEven': 'EstadoEven', 'MotivoCamb': 'MotivoCamb', 'Solicitant': 'Solicitant', 'Solicita_1': 'Solicita_1', 'Solicita_2': 'Solicita_2', 'Solicita_3': 'Solicita_3', 'Solicita_4': 'Solicita_4', 'Solicita_5': 'Solicita_5', 'Solicita_6': 'Solicita_6', 'Solicita_7': 'Solicita_7', 'Solicita_8': 'Solicita_8', 'Solicita_9': 'Solicita_9', 'Solicita10': 'Solicita10', 'Solicita11': 'Solicita11', 'NumeroRecl': 'NumeroRecl', 'NumeroPart': 'NumeroPart', 'UGC': 'UGC', 'Cuadernill': 'Cuadernill', 'Legajo': 'Legajo', 'PartidaMun': 'PartidaMun', 'Partida': 'Partida', 'Dominio': 'Dominio', 'Convenio': 'Convenio', 'Responsabl': 'Responsabl', 'NumeroRefe': 'NumeroRefe', 'result_num': 'result_num', 'osm_id': 'osm_id', 'display_na': 'display_na', 'category': 'category', 'type': 'type', 'latlong': 'latlong', });
+lyr_Calorporzonas_2.set('fieldImages', {'id': 'TextEdit', 'Nº Asist': 'TextEdit', 'limpieza': 'TextEdit', 'espaciosve': 'TextEdit', 'podapreventiva': 'TextEdit', 'podas': 'TextEdit', 'NUMPOINTS': 'TextEdit', });
+lyr_Riegodecalles_3.set('fieldImages', {'Id': '', 'Latitud': '', 'Longitud': '', 'Direccion': '', 'Ubicacion': '', 'Numeracion': '', 'direcc': '', 'pais': '', 'provincia': '', 'Localidad': '', 'TipoAlerta': '', 'Descripcio': '', 'Prioridad': '', 'FechaHoraI': '', 'UsuarioCre': '', 'EsPublico': '', 'EstadoPubl': '', 'Observacio': '', 'Zona': '', 'EstadoEven': '', 'MotivoCamb': '', 'Solicitant': '', 'Solicita_1': '', 'Solicita_2': '', 'Solicita_3': '', 'Solicita_4': '', 'Solicita_5': '', 'Solicita_6': '', 'Solicita_7': '', 'Solicita_8': '', 'Solicita_9': '', 'Solicita10': '', 'Solicita11': '', 'NumeroRecl': '', 'NumeroPart': '', 'UGC': '', 'Cuadernill': '', 'Legajo': '', 'PartidaMun': '', 'Partida': '', 'Dominio': '', 'Convenio': '', 'Responsabl': '', 'NumeroRefe': '', 'result_num': '', 'osm_id': '', 'display_na': '', 'category': '', 'type': '', 'latlong': '', });
+lyr_Calorporzonas_2.set('fieldLabels', {'id': 'no label', 'Nº Asist': 'no label', 'limpieza': 'no label', 'espaciosve': 'no label', 'podapreventiva': 'no label', 'podas': 'no label', 'NUMPOINTS': 'no label', });
+lyr_Riegodecalles_3.set('fieldLabels', {'Id': 'inline label', 'Latitud': 'no label', 'Longitud': 'no label', 'Direccion': 'inline label', 'Ubicacion': 'inline label', 'Numeracion': 'inline label', 'direcc': 'inline label', 'pais': 'no label', 'provincia': 'no label', 'Localidad': 'inline label', 'TipoAlerta': 'inline label', 'Descripcio': 'inline label', 'Prioridad': 'no label', 'FechaHoraI': 'no label', 'UsuarioCre': 'no label', 'EsPublico': 'no label', 'EstadoPubl': 'no label', 'Observacio': 'no label', 'Zona': 'no label', 'EstadoEven': 'no label', 'MotivoCamb': 'no label', 'Solicitant': 'no label', 'Solicita_1': 'no label', 'Solicita_2': 'no label', 'Solicita_3': 'no label', 'Solicita_4': 'no label', 'Solicita_5': 'no label', 'Solicita_6': 'no label', 'Solicita_7': 'no label', 'Solicita_8': 'no label', 'Solicita_9': 'no label', 'Solicita10': 'no label', 'Solicita11': 'no label', 'NumeroRecl': 'no label', 'NumeroPart': 'no label', 'UGC': 'no label', 'Cuadernill': 'no label', 'Legajo': 'no label', 'PartidaMun': 'no label', 'Partida': 'no label', 'Dominio': 'no label', 'Convenio': 'no label', 'Responsabl': 'no label', 'NumeroRefe': 'no label', 'result_num': 'no label', 'osm_id': 'no label', 'display_na': 'no label', 'category': 'no label', 'type': 'no label', 'latlong': 'no label', });
+lyr_Riegodecalles_3.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ mapasCalor/riegodecalles/styles/Calorporzonas_2_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_Calorporzonas_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("NUMPOINTS");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.000000 && value <= 0.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,255,255,0.6)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.000000 && value <= 2.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,191,191,0.6)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 2.000000 && value <= 4.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,128,128,0.6)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 4.000000 && value <= 6.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,64,64,0.6)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 6.000000 && value <= 10.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,0,0,0.6)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
