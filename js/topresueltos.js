1a79c1801b5783bb45ec6ad03d482a932a000e69
ordenamientourbano <87704071+ordenamientourbano@users.noreply.github.com>
2021-08-26 12:58:59 -0300

    Update topresueltos.js

--- js/topresueltos.js
+++ js/topresueltos.js
@@ -590,7 +590,7 @@ var abril = {"mes":[
 ] }
 
 function indicador(e){
-	const resultado = (1+e.Resueltos/e.Total)/(1+Math.pow(1.002, (e.Total-e.Resueltos)));
+	const resultado = (Math.pow(e.resueltos/e.total+0.001,1/5 )/(Math.pow(1.005, (e.Total-e.Resueltos)));
 	return (resultado*10).toFixed(2);
 }
 
