846efeb43956d4dbd5ef4a98438474922acd9a1a
ordenamientourbano <87704071+ordenamientourbano@users.noreply.github.com>
2021-09-04 02:40:31 -0300

    Update topresueltos.js

--- js/topresueltos.js
+++ js/topresueltos.js
@@ -808,6 +808,60 @@ function cargarDesempeño(mes){
     }
 }
 
+
+
+
+function cargarAlertas(mes){
+    var DatosJson = JSON.parse(JSON.stringify(mes));
+    console.log(DatosJson.mes.length);
+    DatosJson.mes.sort(function (a, b) {
+        if (parseFloat(indicador(a)) > parseFloat(indicador(b))) {
+          return 1;
+        }
+        if (parseFloat(indicador(a)) < parseFloat(indicador(b))) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+    document.getElementById("alertas").innerHTML="";
+    $("#Table").append('<thead><th>Área</th>'+
+	'<th>Desempeño</th>' + 
+ 	'<th>Descargar Info</th></thead>');
+    for (i = 0; i < DatosJson.mes.length; i++){
+	 if(indicador(DatosJson.mes[i])<4){
+		 $("#Table").append('
+				    
+				    
+				    
+	               <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                '+DatosJson.mes[i].TipoPuntoEstrategico+'</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+</div>
+				    
+				    
+				    ');
+          }
+    }	      
+}
+
+
+
+
+
+
 function cargarResueltos(mes){
     var DatosJson = JSON.parse(JSON.stringify(mes));
     console.log(DatosJson.mes.length);
